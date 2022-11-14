1d0b43af54bb791a0cf22dd1febc263abc9a4174
Dalidorn <111572685+Dalidorn@users.noreply.github.com>
2022-11-14 11:07:50 -0500

    Revert "Jake feature"

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,7 +46,7 @@ var lunButton = document.querySelector("#lunch");
 var dinButton = document.querySelector("#dinner");
 var dessButton = document.querySelector("#dessert");
 var titleContainer = document.querySelector("#recipeTitle");
-var linkUrl = document.getElementById("linkUrl");
+var ingrContainer = document.getElementById("detailsBlock");
 var recipeImg = document.querySelector("#recipeImg");
 var summary = document.querySelector("#summary");
 var ingredientsli = document.querySelector("#ingredients");
@@ -133,38 +133,6 @@ function selectItem(event) {
   }
 };
 
-function sendMail(name, email, subject, message) {
-  const myHeaders = new Headers();
-  myHeaders.append("Content-Type", "application/json");
-  myHeaders.set('Authorization', 'Basic ' + base64.encode('79ba7f36f96cc9a660a18aa3ef398386'+":" +'6e949ee91b38ae3dc4d9609febb4a90e'));
-
-  const data = JSON.stringify({
-    "Messages": [{
-      "From": {"Email": "<YOUR EMAIL>", "Name": "<YOUR NAME>"},
-      "To": [{"Email": email, "Name": name}],
-      "Subject": subject,
-      "TextPart": message
-    }]
-  });
-
-  const requestOptions = {
-    method: 'POST',
-    headers: myHeaders,
-    body: data,
-  };
-
-  fetch("https://api.mailjet.com/v3.1/send", requestOptions)
-    .then(function(response){
-      return response.text()
-    })
-    .then(function(result){
-      return console.log(result)
-    })
-    .catch(function(error){
-      console.log('error',error);
-    })
-};
-
 function getSelected() {
   document.querySelectorAll(".selected").forEach(fav => {
     selectedFavs.push(user.favorites[fav.id]);
@@ -189,25 +157,13 @@ function fetchRecipeList() {
   var spoonURL = 'https://api.spoonacular.com/recipes/random?apiKey=cc3888f8468f4f98a6465b665303b10b&number=100&tags=';
 
   if (breakButton.className === "active"){
-    tags.push("breakfast,");
+    tags.push("breakfast,")
   } else if (lunButton.className === "active"){
-    tags.push("lunch,");
+    tags.push("lunch,")
   } else if (dinButton.className === "active"){
-    tags.push("dinner,");
+    tags.push("dinner,")
   } else if (dessButton.className === "active"){
-    tags.push("dessert,");
-  }
-
-  if (document.getElementById("vegan").checked === true){
-    tags.push("vegan,");
-  } else if (document.getElementById("vegetarian").checked === true){
-    tags.push("vegetarian,");
-  } else if (document.getElementById("dairyFree").checked === true){
-    tags.push("dairy-free,");
-  } else if (document.getElementById("glutenFree").checked === true){
-    tags.push("gluten-free,");
-  } else if (document.getElementById("keto").checked === true){
-    tags.push("keto,");
+    tags.push("dessert")
   }
 
   for (var i=0; i<tags.length; i++){
@@ -224,17 +180,15 @@ function fetchRecipeList() {
     })
     .then(function (data) {
       console.log(data);
-      currentFetchData = data;
-      // titleContainer.textContent = data.recipes[0].title;
-      // summary.innerHTML = data.recipes[0].summary;
-      // for (var i =0; i<data.recipes[0].extendedIngredients.length; i++){
-      //   var list = document.createElement('li');
-      //   list.innerHTML = data.recipes[0].extendedIngredients[i].original;
-      //   ingredientsli.appendChild(list);
-      // }
-      // recipeImg.src = data.recipes[0].image;
-      // console.log(data.recipes[0].spoonacularSourceUrl)
-      // linkUrl.textContent = data.recipes[0].spoonacularSourceUrl
+      titleContainer.textContent = data.recipes[0].title;
+      summary.innerHTML = data.recipes[0].summary;
+      for (var i =0; i<data.recipes[0].extendedIngredients.length; i++){
+        var list = document.createElement('li');
+        list.innerHTML = data.recipes[0].extendedIngredients[i].original;
+        ingredientsli.appendChild(list);
+      }
+      recipeImg.src = data.recipes[0].image;
+      console.log(data.recipes[0].spoonacularSourceUrl)
     });
 };
 
@@ -243,16 +197,6 @@ var recipeIncr = 0;
 
 //---RECIPE CARD FUNCTIONS---
 function nextRecipe() {
-  titleContainer.textContent = currentFetchData.recipes[recipeIncr].title;
-  summary.innerHTML = currentFetchData.recipes[recipeIncr].summary;
-  for (var i =0; i<currentFetchData.recipes[recipeIncr].extendedIngredients.length; i++){
-    var list = document.createElement('li');
-    list.innerHTML = currentFetchData.recipes[recipeIncr].extendedIngredients[i].original;
-    ingredientsli.appendChild(list);
-    }
-  recipeImg.src = currentFetchData.recipes[recipeIncr].image;
-  console.log(currentFetchData.recipes[recipeIncr].spoonacularSourceUrl)
-  linkUrl.textContent = currentFetchData.recipes[recipeIncr].spoonacularSourceUrl
   recipeIncr ++
   // fetchRecipe()
   console.log("SHOW " + recipeIncr + "RECIPE");
@@ -272,14 +216,12 @@ function favRecipe() {
 };
 
 function showRecipeDetails() {
-  if(linkUrl.className == "invisible") {
-    linkUrl.className = "";
+  if(detailsBlock.className == "invisible") {
+    detailsBlock.className = "";
   } else {
-    linkUrl.className = "invisible"
+    detailsBlock.className = "invisible"
   };
 };
-
-
 
 //---DRAG AND DROP FUNCTIONALITY---
 const position = { x: 0, y: 0 };
